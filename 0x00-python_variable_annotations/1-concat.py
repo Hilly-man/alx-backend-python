f6a40ba07e1146d8243d7f1b4f9c061edc054330
Hilly-man <hilkiahe@gmail.com>
2024-01-11 23:23:16 +0000

    Basic annotation concat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+    """
+    Concatenates two strings and returns the result.
+
+    Parameters:
+    str1 (str): The first input string.
+    str2 (str): The second input string.
+
+    Returns:
+    str: The concatenated string.
+    """
+
+def concat(str1: str, str2: str) -> str:
+
+    return str1 + str2
